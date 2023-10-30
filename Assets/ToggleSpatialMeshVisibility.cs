efa5b5a91575ee90c816071d025b6fdc8374227c
MoritzSkrepek <skrepek.moritz@student.htlwrn.ac.at>
2023-10-30 14:47:52 +0100

    Basic project

---
+++ Assets/ToggleSpatialMeshVisibility.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using TMPro;
+using MixedReality.Toolkit.UX;
+
+public class ToggleSpatialMeshVisibility : MonoBehaviour
+{
+    public GameObject mrtkXRRig;
+    public PressableButton button;
+
+    private bool isArMeshVisible = true;
+    private ARMeshManager arMeshManager;
+    private TextMeshProUGUI buttonTMPro;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (button != null)
+        {
+            button.OnClicked.AddListener(ToggleVisibility);
+            UpdateButton();
+        }
+
+        arMeshManager = mrtkXRRig.GetComponentInChildren<ARMeshManager>();
+        buttonTMPro = button.GetComponentInChildren<TextMeshProUGUI>();
+
+        Debug.Log(mrtkXRRig.gameObject.name);
+        Debug.Log(arMeshManager.gameObject.name);
+        Debug.Log(button.gameObject.name);
+    }
+
+    void ToggleVisibility()
+    {
+        Debug.Log("Toggle");
+        isArMeshVisible = !isArMeshVisible;
+
+        if (arMeshManager != null)
+        {
+            arMeshManager.enabled = isArMeshVisible;
+        }
+
+        UpdateButton();
+    }
+
+    void UpdateButton()
+    {
+        if (button != null && buttonTMPro != null)
+        {
+            string textValue = buttonTMPro.text;
+            Debug.Log("Button Text (TextMeshPro): " + textValue);
+
+            buttonTMPro.text = isArMeshVisible ? "Turn off Mesh" : "Turn on Mesh";
+        }
+    }
+}
