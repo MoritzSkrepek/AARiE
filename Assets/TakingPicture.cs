981d70ae7076b215daa28b37d233cf58da23c62c
Jonas Schoditsch <schoditsch.jonas@student.htlwrn.ac.at>
2023-11-04 16:58:33 +0100

    Taking pictures, and editing them
    Taking a picture every 5 seconds, and eidting it so only the red pixels show

--- Assets/TakingPicture.cs
+++ Assets/TakingPicture.cs
@@ -1,5 +1,5 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Windows.WebCam;
@@ -9,9 +9,12 @@ public class TakingPicture : MonoBehaviour
     // Start is called before the first frame update
     PhotoCapture photoCaptureObject = null;
     Texture2D targetTexture = null;
+    CameraParameters cameraParameters = new CameraParameters();
+    private bool isCapturing = false;
+    private bool isCameraActive = false;
 
     // Use this for initialization
-    public void Start()
+    void Start()
     {
         Debug.Log("Start");
         Resolution cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
@@ -19,22 +22,19 @@ public class TakingPicture : MonoBehaviour
 
         // Create a PhotoCapture object
         PhotoCapture.CreateAsync(false, delegate (PhotoCapture captureObject) {
-        if (captureObject != null)
-        {
-            photoCaptureObject = captureObject;
-            CameraParameters cameraParameters = new CameraParameters();
-            cameraParameters.hologramOpacity = 0.0f;
-            cameraParameters.cameraResolutionWidth = cameraResolution.width;
-            cameraParameters.cameraResolutionHeight = cameraResolution.height;
-            cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
+            if (captureObject != null)
+            {
+                photoCaptureObject = captureObject;
+                cameraParameters.hologramOpacity = 0.0f;
+                cameraParameters.cameraResolutionWidth = cameraResolution.width;
+                cameraParameters.cameraResolutionHeight = cameraResolution.height;
+                cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
 
-            // Activate the camera
-            photoCaptureObject.StartPhotoModeAsync(cameraParameters, delegate (PhotoCapture.PhotoCaptureResult result) {
-                Debug.Log("Camera Activated");
-                // Take a picture
-                photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
-                Debug.Log("Picture taken");
-            });
+                // Activate the camera
+                photoCaptureObject.StartPhotoModeAsync(cameraParameters, delegate (PhotoCapture.PhotoCaptureResult result) {
+                    isCameraActive = true;
+                    Debug.Log("Camera Activated");
+                });
             }
             else
             {
@@ -43,7 +43,7 @@ public class TakingPicture : MonoBehaviour
         });
     }
 
-    void OnCapturedPhotoToMemory(PhotoCapture.PhotoCaptureResult result, PhotoCaptureFrame photoCaptureFrame)
+    void onCapturedPhotoToMemory(PhotoCapture.PhotoCaptureResult result, PhotoCaptureFrame photoCaptureFrame)
     {
         // Copy the raw image data into the target texture
         photoCaptureFrame.UploadImageDataToTexture(targetTexture);
@@ -52,6 +52,7 @@ public class TakingPicture : MonoBehaviour
         Debug.Log("Render Texture to Gameobject");
         if (targetTexture != null)
         {
+            targetTexture = editTexture(targetTexture);
             Renderer rend = GetComponent<Renderer>();
             rend.material.mainTexture = targetTexture;
         }
@@ -59,22 +60,96 @@ public class TakingPicture : MonoBehaviour
         {
             Debug.LogError("New texture is not assigned!");
         }
-
-        // Deactivate the camera
-        photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
-        Debug.Log("Camera deactivated");
     }
 
-    void OnStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result)
+    void onStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result)
     {
         // Shutdown the photo capture resource
         photoCaptureObject.Dispose();
         photoCaptureObject = null;
     }
 
+    Texture2D editTexture(Texture2D textureToEdit)
+    {
+        // Get the pixel data from the texture
+        Color[] pixels = textureToEdit.GetPixels();
+
+        for (int i = 0; i < pixels.Length; i++)
+        {
+            // Check if the pixel is predominantly red (you can adjust the threshold)
+            if (pixels[i].r > 0.65f && pixels[i].g < 0.5f && pixels[i].b < 0.5f)
+            {
+                pixels[i] = Color.red;
+            }
+            else
+            {
+                pixels[i] = Color.black; // Set non-red pixels to black
+            }
+        }
+
+        // Apply the edited pixels back to the texture
+        textureToEdit.SetPixels(pixels);
+        textureToEdit.Apply();
+        return textureToEdit;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+    }
+    public void isToggled()
+    {
+        if (photoCaptureObject != null && !isCapturing)
+        {
+            if (!isCameraActive)
+            {
+                photoCaptureObject.StartPhotoModeAsync(cameraParameters, delegate (PhotoCapture.PhotoCaptureResult result) {
+                    if (result.success)
+                    {
+                        isCameraActive = true;
+                        Debug.Log("Camera Activated");
+                        isCapturing = true;
+                        StartCoroutine(capturePhotosRepeatedly());
+                    }
+                    else
+                    {
+                        Debug.LogError("Failed to activate the camera: " + result.hResult);
+                    }
+                });
+            }
+        }
+    }
+
+    public void isUnToggled()
+    {
+        if (isCameraActive)
+        {
+            isCapturing = false;
+            // Deactivate the camera
+            photoCaptureObject.StopPhotoModeAsync(onStoppedPhotoMode);
+            isCameraActive = false;
+            Debug.Log("Camera deactivated");
+        }
+    }
+
+    private IEnumerator capturePhotosRepeatedly()
+    {
+        while (isCapturing)
+        {
+            photoCaptureObject.TakePhotoAsync((result, photoCaptureFrame) =>
+            {
+                if (result.success)
+                {
+                    Debug.Log("Picture taken");
+                    onCapturedPhotoToMemory(result, photoCaptureFrame);
+                }
+                else
+                {
+                    Debug.LogError("Failed to take a picture: " + result.hResult);
+                }
+            });
+
+            yield return new WaitForSeconds(5.0f); // Wait for 5 seconds before taking the next picture
+        }
     }
 }
