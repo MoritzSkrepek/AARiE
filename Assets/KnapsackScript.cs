4ddd2dfe1160a071a6373bf1a2c1ba8d010fc347
MoritzSkrepek <skrepek.moritz@student.htlwrn.ac.at>
2023-11-24 08:04:33 +0100

    Deleted old code

--- Assets/KnapsackScript.cs
+++ Assets/KnapsackScript.cs
@@ -1,114 +1,3 @@
-/*using System;
-using System.Collections.Generic;
-using TMPro;
-using UnityEngine;
-
-public class KnapsackScript : MonoBehaviour
-{
-    [Serializable]
-    public class Item
-    {
-        public int ID;
-        public int Value;
-        public int Weight;
-        public int Width;
-        public int Height;
-    }
-
-    //private int[,] inventory;
-    private int[,] inventory = new int[7, 7]
-    { { 1, 0, 0, 0, 0, 0, 0 },
-      { 2, 3, 0, 0, 0, 0, 0 },
-      { 0, 0, 0, 0, 0, 0, 0 },
-      { 0, 0, 0, 0, 0, 0, 0 },
-      { 0, 0, 0, 8, 0, 0, 0 },
-      { 0, 0, 0, 0, 0, 0, 0 },
-      { 0, 0, 9, 0, 0, 0, 0 } };
-
-    public TextMeshPro ownVal;
-    public TextMeshPro maxVal;
-
-    //Just for test purposes right now
-    public Dictionary<int, Item> items = new Dictionary<int, Item>
-    {
-        {1, new Item { ID = 1, Value = 50, Weight = 25}},
-        {2, new Item { ID = 2, Value = 20, Weight = 10}},
-        {3, new Item { ID = 3, Value = 10, Weight = 5}},
-        {4, new Item { ID = 4, Value = 25, Weight = 13}},
-        {5, new Item { ID = 5, Value = 40, Weight = 20}},
-        {6, new Item { ID = 6, Value = 45, Weight = 22}},
-        {7, new Item { ID = 7, Value = 5, Weight = 1}},
-        {8, new Item { ID = 8, Value = 100, Weight = 50}},
-        {9, new Item { ID = 9, Value = 30, Weight = 6}},
-        {10, new Item { ID = 10, Value = 15, Weight = 8}}
-    };
-
-    public void Start()
-    {
-        Debug.Log("Shalom");
-        int capacity = 120;
-        int maxValue = KnapsackMaxValue(items, capacity);
-        Debug.Log("Maximaler Wert des Knapsacks: " + maxValue);
-        maxVal.text += " " + maxValue.ToString();
-
-        int inventoryValue = KnapsackInventoryValue(items, inventory);
-        Debug.Log("Wert des Inventars: " + inventoryValue);
-        ownVal.text += " " + inventoryValue.ToString();
-
-    }
-
-    int KnapsackMaxValue(Dictionary<int, Item> items, int capacity)
-    {
-        int n = items.Count;
-        int[,] dp = new int[n + 1, capacity + 1];
-
-        for (int i = 0; i <= n; i++)
-        {
-            for (int w = 0; w <= capacity; w++)
-            {
-                if (i == 0 || w == 0)
-                    dp[i, w] = 0;
-                else if (items[i].Weight <= w)
-                    dp[i, w] = Mathf.Max(items[i].Value + dp[i - 1, w - items[i].Weight], dp[i - 1, w]);
-                else
-                    dp[i, w] = dp[i - 1, w];
-            }
-        }
-
-        return dp[n, capacity];
-    }
-
-    int KnapsackInventoryValue(Dictionary<int, Item> items, int[,] inventory)
-    {
-        int totalValue = 0;
-
-        for (int i = 1; i <= items.Count; i++)
-        {
-            int itemValue = items[i].Value;
-
-            for (int j = 0; j < inventory.GetLength(0); j++)
-            {
-                for (int k = 0; k < inventory.GetLength(1); k++)
-                {
-                    if (inventory[j, k] == i)
-                    {
-                        totalValue += itemValue;
-                    }
-                }
-            }
-        }
-
-        return totalValue;
-    }
-
-
-    public void SetInventory(int[,] newInventory)
-    {
-        inventory = newInventory;
-    }
-}
-*/
-
 using System;
 using System.Collections.Generic;
 using TMPro;
@@ -126,9 +15,8 @@ public class KnapsackScript : MonoBehaviour
 
     public TextMeshPro ownVal;
     public TextMeshPro maxVal;
-    private GameObject inventoryObject;
-    // This is the inventory that is going to be set
     private int[,] inventory;
+    private int[,] usedItems;
 
     // inventory for calculation testing
     /*private int[,] inventory = new int[3, 3]
@@ -150,9 +38,7 @@ public class KnapsackScript : MonoBehaviour
         {8, new Item { ID = 8, Value = 100, Weight = 50}},
         {9, new Item { ID = 9, Value = 30, Weight = 6}},
         {10, new Item { ID = 10, Value = 15, Weight = 8}}
-    };
-
-    private int[,] usedItems;
+    };  
 
     void Start()
     {
@@ -176,7 +62,7 @@ public class KnapsackScript : MonoBehaviour
                     Debug.Log(". ");
                 }
             }
-            Debug.Log(""); // This creates a new line after each row
+            Debug.Log("");
         }
 
         Debug.Log("Own Items:");
@@ -193,7 +79,7 @@ public class KnapsackScript : MonoBehaviour
                     Debug.Log(". ");
                 }
             }
-            Debug.Log(""); // This creates a new line after each row
+            Debug.Log("");
         }
 
         int inventoryValue = KnapsackInventoryValue(items, inventory);
@@ -309,10 +195,5 @@ public class KnapsackScript : MonoBehaviour
     public void SetInventory(int[,] newInventory)
     {
         inventory = newInventory;
-    }
-
-    public void SetInventoryGridObject(GameObject inventoryObj)
-    {
-        inventoryObject = inventoryObj;
     }
 }
