24286cc0f53ec5fa2535132c2d1574cd5b46e336
SerefEfe1 <Haylaz.Seref@student.htlwrn.ac.at>
2024-02-16 12:48:39 +0100

    Needs testing

--- Assets/Scripts/RequestManager.cs
+++ Assets/Scripts/RequestManager.cs
@@ -1,12 +1,10 @@
 using System;
 using System.Collections;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Net.Sockets;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -16,34 +14,33 @@ public class RequestManager : MonoBehaviour
     private string[] laptops = new string[2];
     private int port = 9090;
 
-    void Start()
+    private void Start()
     {
         listener = new HttpListener();
-        listener.Prefixes.Add($"http://*:{this.port}/");
-
-        EventManager.OnMessageSend += sendMsg;
-
+        listener.Prefixes.Add($"http://*:{port}/");
         listener.Start();
 
-        Thread listenerThread = new Thread(listen);
+        Thread listenerThread = new Thread(Listen);
         listenerThread.Start();
     }
 
-    void Update()
-    {
-
-    }
-
-    private void listen()
+    private void Listen()
     {
         while (true)
         {
-            HttpListenerContext context = listener.GetContext();
-            Task.Run(() => HandleRequest(context));
+            try
+            {
+                HttpListenerContext context = listener.GetContext();
+                Task.Run(() => HandleRequest(context));
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"Error handling request: {e.Message}");
+            }
         }
     }
 
-    private void HandleRequest(HttpListenerContext context)
+    private async void HandleRequest(HttpListenerContext context)
     {
         try
         {
@@ -55,55 +52,25 @@ public class RequestManager : MonoBehaviour
                 string requestBody;
                 using (StreamReader reader = new StreamReader(context.Request.InputStream))
                 {
-                    requestBody = reader.ReadToEnd();
+                    requestBody = await reader.ReadToEndAsync();
                 }
 
                 if (url == "/register")
                 {
-                    if (!laptops.Contains(requestBody))
-                    {
-                        if (laptops[0] == null)
-                        {
-                            laptops[0] = requestBody;
-                        }
-                        else if (laptops[1] == null)
-                        {
-                            laptops[1] = requestBody;
-                        }
-                        else
-                        {
-                            throw new Exception("Too many IP addresses registered");
-                        }
-                    }
+                    RegisterLaptop(requestBody);
                 }
                 else if (url == "/message")
                 {
-                    MessageData requestData = JsonUtility.FromJson<MessageData>(requestBody);
-                    string username = requestData.username;
-                    string message = requestData.message;
-                    if (laptops[0] == context.Request.UserHostAddress)
-                    {
-                        EventManager.ReceiveMsg(0, username, message);
-                        Debug.Log("Received message from 0: " + context.Request.UserHostAddress);
-                    }
-                    else if (laptops[1] == context.Request.UserHostAddress)
-                    {
-                        EventManager.ReceiveMsg(1, username, message);
-                        Debug.Log("Received message from 1: " + context.Request.UserHostAddress);
-                    }
+                    ProcessMessage(requestBody, context.Request.UserHostAddress);
                 }
 
-                string responseString = "200 OK";
-                byte[] responseBytes = Encoding.UTF8.GetBytes(responseString);
-                context.Response.ContentType = "text/plain";
-                context.Response.ContentLength64 = responseBytes.Length;
-                context.Response.OutputStream.Write(responseBytes, 0, responseBytes.Length);
+                Respond(context, HttpStatusCode.OK, "OK");
             }
         }
         catch (Exception e)
         {
-            Console.WriteLine("Error: " + e.Message);
-            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            Debug.LogError($"Error handling request: {e.Message}");
+            Respond(context, HttpStatusCode.InternalServerError, "Internal Server Error");
         }
         finally
         {
@@ -111,39 +78,96 @@ public class RequestManager : MonoBehaviour
         }
     }
 
-
-    public void sendMsg(int idx, string username, string message)
+    private void RegisterLaptop(string requestBody)
     {
-        StartCoroutine(sendMessage(idx, username, message));
+        if (!string.IsNullOrEmpty(requestBody))
+        {
+            for (int i = 0; i < laptops.Length; i++)
+            {
+                if (string.IsNullOrEmpty(laptops[i]))
+                {
+                    laptops[i] = requestBody;
+                    break;
+                }
+            }
+        }
     }
 
-    private IEnumerator sendMessage(int idx, string username, string message)
+    private void ProcessMessage(string requestBody, string senderIpAddress)
     {
-        string jsonPayload = "{\"username\": \"" + username + "\", \"message\": \"" + message + "\"}";
+        if (!string.IsNullOrEmpty(requestBody))
+        {
+            MessageData messageData = JsonUtility.FromJson<MessageData>(requestBody);
+            string username = messageData.username;
+            string message = messageData.message;
+
+            for (int i = 0; i < laptops.Length; i++)
+            {
+                if (laptops[i] == senderIpAddress)
+                {
+                    Debug.Log($"Received message from {i}: {username} - {message}");
+
+                    for (int j = 0; j < laptops.Length; j++)
+                    {
+                        if (laptops[j] != senderIpAddress && !string.IsNullOrEmpty(laptops[j]))
+                        {
+                            SendMsg(j, username, message);
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    private void Respond(HttpListenerContext context, HttpStatusCode statusCode, string responseMessage)
+    {
+        byte[] responseBytes = System.Text.Encoding.UTF8.GetBytes(responseMessage);
+        context.Response.StatusCode = (int)statusCode;
+        context.Response.ContentType = "text/plain";
+        context.Response.ContentLength64 = responseBytes.Length;
+        context.Response.OutputStream.Write(responseBytes, 0, responseBytes.Length);
+        if (context.Request.HttpMethod == "OPTIONS")
+        {
+            context.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+            context.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST");
+            context.Response.AddHeader("Access-Control-Max-Age", "1728000");
+        }
+        context.Response.AppendHeader("Access-Control-Allow-Origin", "*");
+    }
+
+    public void SendMsg(int idx, string username, string message)
+    {
+        StartCoroutine(SendMessage(idx, username, message));
+    }
+
+    private IEnumerator SendMessage(int idx, string username, string message)
+    {
+        string jsonPayload = JsonUtility.ToJson(new MessageData { username = username, message = message });
 
         string ipAddress = laptops[idx];
-        string url = "http://" + ipAddress + ":9090/message";
+        string url = $"http://{ipAddress}:9090/message";
 
         using (UnityWebRequest request = UnityWebRequest.PostWwwForm(url, jsonPayload))
         {
             request.SetRequestHeader("Content-Type", "application/json");
 
             byte[] jsonBytes = System.Text.Encoding.UTF8.GetBytes(jsonPayload);
-
             request.uploadHandler = new UploadHandlerRaw(jsonBytes);
-
             request.downloadHandler = new DownloadHandlerBuffer();
 
             yield return request.SendWebRequest();
 
             if (request.result != UnityWebRequest.Result.Success)
             {
-                Debug.LogError("Error sending message: " + request.error);
+                Debug.LogError($"Error sending message: {request.error}");
             }
         }
     }
 
-    private string getIp()
+
+    private string GetIp()
     {
         string ipv4Address = "";
         string hostName = Dns.GetHostName();
@@ -161,22 +185,15 @@ public class RequestManager : MonoBehaviour
         return ipv4Address;
     }
 
-    public void stopListener()
+    public void StopListener()
     {
         listener.Stop();
     }
 }
-
 
 [Serializable]
 public class MessageData
 {
     public string username;
     public string message;
-
-    public MessageData(string username, string message)
-    {
-        this.username = username;
-        this.message = message;
-    }
 }
