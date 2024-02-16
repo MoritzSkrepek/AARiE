456d921e880739d6ec4facf853f12676aad4dbf2
dxstin <lampel.dustin@student.htlwrn.ac.at>
2024-02-16 10:57:15 +0100

    debug und menuskript verschoben
    kann nichtmehr arbeiten heute hehe

--- Assets/Scripts/Menu_Spawn.cs
+++ Assets/Scripts/Menu_Spawn.cs
@@ -13,14 +13,17 @@ public class SpawnPrefab : MonoBehaviour
 
     void SpawnPrefabMenu()
     {
+        Debug.Log("MURUK 1");
         // Holen Sie die aktuelle Kameraposition
         Vector3 cameraPosition = Camera.main.transform.position;
-
+        Debug.Log("MURUK 2");
         // Versetzen Sie das Prefab-Menü um 1 auf der Z-Achse
         Vector3 spawnPosition = new Vector3(cameraPosition.x, cameraPosition.y, cameraPosition.z + 1f);
-
+        Debug.Log("MURUK 3");
         // Spawnt das Prefab-Menü an der neuen Position
         Instantiate(menu, spawnPosition, Quaternion.identity);
+        Debug.Log("MURUK 4");
         menu.SetActive(true);
+        Debug.Log("MURUK 5");
     }
 }
