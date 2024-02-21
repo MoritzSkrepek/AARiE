5c1f8049bae3b1c9701fc8c3719a89385069dc15
dxstin <lampel.dustin@student.htlwrn.ac.at>
2024-02-21 11:26:26 +0100

    Potentieller fix hihi

--- Assets/Scripts/Menu_Spawn.cs
+++ Assets/Scripts/Menu_Spawn.cs
@@ -13,17 +13,9 @@ public class SpawnPrefab : MonoBehaviour
 
     void SpawnPrefabMenu()
     {
-        Debug.Log("MURUK 1");
-        // Holen Sie die aktuelle Kameraposition
         Vector3 cameraPosition = Camera.main.transform.position;
-        Debug.Log("MURUK 2");
-        // Versetzen Sie das Prefab-Menü um 1 auf der Z-Achse
         Vector3 spawnPosition = new Vector3(cameraPosition.x, cameraPosition.y, cameraPosition.z + 1f);
-        Debug.Log("MURUK 3");
-        // Spawnt das Prefab-Menü an der neuen Position
-        Instantiate(menu, spawnPosition, Quaternion.identity);
-        Debug.Log("MURUK 4");
-        menu.SetActive(true);
-        Debug.Log("MURUK 5");
+        GameObject instantiatedObject =  Instantiate(menu, spawnPosition, Quaternion.identity);
+        instantiatedObject.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
     }
 }
