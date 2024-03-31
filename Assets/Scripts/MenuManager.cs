7526227152b93f42793df131f30bd689a0f9af03
dxstin <lampel.dustin@student.htlwrn.ac.at>
2024-03-31 10:12:43 +0200

    menulvdescr fix

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,13 +1,10 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuManager : MonoBehaviour
-{
+public class MenuManager : MonoBehaviour{
     public GameObject menuPrefab;
     public Transform cameraPosition;
-
-    private void Awake()
-    {
+    private void Awake(){
         Vector3 newSpawnPoint = new Vector3(cameraPosition.position.x, cameraPosition.position.y, cameraPosition.position.z + 1);
         menuPrefab.transform.position = newSpawnPoint;
         menuPrefab.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
